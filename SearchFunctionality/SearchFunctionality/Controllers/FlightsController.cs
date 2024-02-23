c7f880572a5f3309c9a7593f6dc27fb40b2d4316
Divya Gupta <divyag@techdimension.in>
2024-02-23 17:29:57 +0530

    Added athorize to flight controller

--- SearchFunctionality/SearchFunctionality/Controllers/FlightsController.cs
+++ SearchFunctionality/SearchFunctionality/Controllers/FlightsController.cs
@@ -6,7 +6,7 @@ using SearchFunctionality.BusinessLogic.Services;
 
 namespace SearchFunctionality.Controllers
 {
-    //[Authorize]
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class FlightsController : ControllerBase
